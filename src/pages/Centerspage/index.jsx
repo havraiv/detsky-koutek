a3bab807465cb1fc4ce9a12688a2183b993a1ab5
Iva Havranová <havranova.i@seznam.cz>
2023-07-10 19:35:40 +0200

    CentersPage component created

--- src/pages/Centerspage/index.jsx
+++ src/pages/Centerspage/index.jsx
@@ -1,1 +1,19 @@
+import React from 'react';
+import { getAllCenters } from '../../centers';
+import { Link } from 'react-router-dom';
 
+export const CentersPage = () => {
+  return (
+    <>
+      <ul>
+        {getAllCenters().map((center) => {
+          return (
+            <li key={center.id}>
+              <Link to={`/pobocky/${center.id}`}>{center.name}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
