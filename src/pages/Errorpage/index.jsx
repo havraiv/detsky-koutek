9347e4b4f3e2e51cae922a14fec959fc2cc854ce
Iva Havranová <havranova.i@seznam.cz>
2023-07-10 19:26:18 +0200

    Error page - link Home added, centerspage components deleted

--- src/pages/Errorpage/index.jsx
+++ src/pages/Errorpage/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const ErrorPage = () => {
   return (
     <>
       <h1>404</h1>
-      <p>Zde nic není</p>
+      <p>
+        Zde nic není, vrať se zpět <Link to="/home">domů</Link>
+      </p>
     </>
   );
 };
